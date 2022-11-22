b89202034b3ad22e92cf2c8d6b8d48f1772ce667
Oleg <olegvpc@yandex.ru>
2022-11-22 20:39:25 +0700

    fix: corrected background-color of Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
+import { useLocation } from 'react-router-dom'
 import './Header.css';
 import Logo from '../Logo/Logo';
 import Navigation from '../Navigation/Navigation';
 
 function Header({ loggedIn }) {
 
+  const mainSite = useLocation().pathname === "/" ? true : false
+
   return (
-    <header className={`header ${!loggedIn && "header_main"}`}>
+    <header className={`header ${(!loggedIn || mainSite )&& "header_main"}`}>
         <Logo />
         <Navigation loggedIn={loggedIn}/>
     </header>
